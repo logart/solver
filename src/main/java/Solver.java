6d2d245f5d652a6ee33960537af2cb663c7a399c
Artem Loginov <logart2007@gmail.com>
2013-03-16 19:23:19 +0200

    reformat code Solver.java

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -6,38 +6,38 @@ import org.apache.commons.math.linear.RealMatrix;
 import java.util.Random;
 
 public class Solver {
-  private double epsilon = 0.75;
-  private double h = 0.1;
-  double omega = h / (h + epsilon);
-  MultivariateVectorialFunction G;
+    private double epsilon = 0.75;
+    private double h = 0.1;
+    double omega = h / (h + epsilon);
+    MultivariateVectorialFunction G;
 
-  public Solver(MultivariateVectorialFunction multivariateMatrixFunction) {
-    G = multivariateMatrixFunction;
-  }
+    public Solver(MultivariateVectorialFunction multivariateMatrixFunction) {
+        G = multivariateMatrixFunction;
+    }
 
 
-  public void solve() throws FunctionEvaluationException {
-    RealMatrix Xn = new Array2DRowRealMatrix(new double[]{0.0, 0.0});
-    //    Z0 = h*G(X0);
-    RealMatrix Zn = new Array2DRowRealMatrix(G.value(Xn.getColumn(0))).scalarMultiply(h);
-    int i = 0;
-    while (i < 100) {
+    public void solve() throws FunctionEvaluationException {
+        RealMatrix Xn = new Array2DRowRealMatrix(new double[]{0.0, 0.0});
+        //    Z0 = h*G(X0);
+        RealMatrix Zn = new Array2DRowRealMatrix(G.value(Xn.getColumn(0))).scalarMultiply(h);
+        int i = 0;
+        while (i < 100) {
 //      RealMatrix Xnext0 = Xn.add(Zn);
 
-      Array2DRowRealMatrix valueOfFunctionInPoint = new Array2DRowRealMatrix(G.value(Xn.getColumn(0)));
-      RealMatrix valueOfFunctionInPointWithZn = (valueOfFunctionInPoint.scalarMultiply(epsilon)).add(Zn);
-      RealMatrix Znext = valueOfFunctionInPointWithZn.scalarMultiply(omega);
-      RealMatrix Xnext = Xn.add(Znext);
+            Array2DRowRealMatrix valueOfFunctionInPoint = new Array2DRowRealMatrix(G.value(Xn.getColumn(0)));
+            RealMatrix valueOfFunctionInPointWithZn = (valueOfFunctionInPoint.scalarMultiply(epsilon)).add(Zn);
+            RealMatrix Znext = valueOfFunctionInPointWithZn.scalarMultiply(omega);
+            RealMatrix Xnext = Xn.add(Znext);
 
-      Zn = Znext;
-      Xn = Xnext;
-      ++i;
+            Zn = Znext;
+            Xn = Xnext;
+            ++i;
 
-      for (double v : Xn.getColumn(0)) {
-        System.out.print(v + " ");
-      }
-      System.out.println("\n");
+            for (double v : Xn.getColumn(0)) {
+                System.out.print(v + " ");
+            }
+            System.out.println("\n");
 
+        }
     }
-  }
 }
